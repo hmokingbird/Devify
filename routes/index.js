a1e0db2f0952d58169dfaed30e7944e40a37516e
Thefish1996 <rfishkin1313@gmail.com>
2018-12-11 16:39:37 -0500

    commented specific details of route paths

--- routes/index.js
+++ routes/index.js
@@ -3,33 +3,32 @@ let querystring = require('querystring');
 let rp = require('request-promise');
 const user= require("../data")
 
-const userInfo = user.userData
 
 const constructorMethod = app => {
   
-  let client_id = '0edee0583a08407fa148378bb88dcf68'; // Your client id
-  let client_secret = '7807b53ecdff4da3a2325ce589b798d2'; // Your secret
-  let redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
+  let client_id = '0edee0583a08407fa148378bb88dcf68'; // Your client id thats provided form our application
+  let client_secret = '7807b53ecdff4da3a2325ce589b798d2'; // Your secret id thats provided form our application
+  let redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri thats added to our app via spotify. If the redirect uri isnt added to spotify acount app, it wont work
   let authorized = false;
 
   app.get('/', (req, res) => {
-    res.render("authentication/static", {})
+    res.render("authentication/static", {})    // default path thats just login
   })
 
-  app.get('/login', function(req, res) {
+  app.get('/login', function(req, res) {                         //path that hits the authorization path of spotify
     res.redirect('https://accounts.spotify.com/authorize?' +
       querystring.stringify({
         response_type: 'code',
         client_id: client_id,
-        scope: 'user-read-private user-read-email',
-        redirect_uri
+        scope: 'user-read-private user-read-email',        //read rights that sptofy authorizes after successful login
+        redirect_uri                                     //redirect uri after successful authorization.                              
       }))
   })
   
-  app.get('/callback', function(req, res) {
+  app.get('/callback', function(req, res) {         //redirect uri path  // IF YOU HIT THIS PATH THERE IS AN ERROR AKA ITS A CALLBACK, AND IT NEVER REDIRECTS THROUGH LOGIN
     authorized = true
     let code = req.query.code || null
-    let authOptions = {
+    let authOptions = {                               //this object is to retrieve a token from the spotify after a sucessful login.
       url: 'https://accounts.spotify.com/api/token',
       form: {
         code: code,
@@ -41,56 +40,37 @@ const constructorMethod = app => {
       },
       json: true
     }
-    request.post(authOptions, function(error, response, body) {
-      let access_token = body.access_token
+    request.post(authOptions, function(error, response, body) {   //this request takes the object and hits sptofys api which in the response of the body returns the token
+      let access_token = body.access_token    //access token taken from body
 
-      // const options = {                                             //this is your request from the spotify web api the get requests that are happening
-      //   url: 'https://api.spotify.com/v1/me',
-      //   headers: { 'Authorization': 'Bearer ' + access_token },
-      //   json: true
-      // };
+      let uri = 'http://localhost:3000/logged'               //uri that is used after a successful callback and its our "logged" page
+      res.redirect(uri + '?access_token=' + access_token)    // after the access token is gotten it will redirect to the the "logged" page with the access token in the header
+      //IMPORTANT: ACCESS TOKEN HAS A 1 HOUR TIMEOUT PER SPOTIFY
 
-      // // use the access token to access the Spotify Web API
-      // request.get(options, async function(error, response, body) {
-      //   try{
-      //   console.log(/*body.display_name*/);
-      //   let findUser = await userInfo.getUser(body.display_name) //checks if users unique spotify name already exists
-      //   if(!findUser){
-      //     await userInfo.addUser(body.display_name)  //if not adds user
-      //   }
-      //   }
-      //   catch (error){
-      //     throw error
-      //   }
-      // });
-
-      let uri = 'http://localhost:3000/logged'
-      res.redirect(uri + '?access_token=' + access_token)
-
+      // FOR EAKRUM. This access token is only part of the logged header and needs to be checked for all. If the access token exists that means the user successfully logged in through spotify authentication
     })
   }) 
   
-  app.get("/logged",async function (req,res) {
-    let access_token = req.query.access_token
-    const options = {
+  app.get("/logged",async function (req,res) {    //logged path
+    let access_token = req.query.access_token    //pulls access token from header. If possible maybe make the access token a cookie?
+    const options = {                             //object used to hit spotifys api to get the data we need
       method: 'GET',
       uri: 'https://api.spotify.com/v1/me',
       headers: { 'Authorization': 'Bearer ' + access_token },
       json: true,  
     }
-      // use the access token to access the Spotify Web API
-    let data = await rp(options)
+    let data = await rp(options)      //this will return a promise passing in the options object and returning the resulting data
     console.log(data)
+
     // //body.tracks.items[i].whatever property you need
           // /*body.display_name*/);
 
 
-    let profilePicture = "/public/img/no-profile-picture-icon.jpg"     //if the image aray is zero that means there is no image and should default to this
+    let profilePicture = "/public/img/no-profile-picture-icon.jpg"     //if the image array is zero that means there is no image and should default to this
     if(data.images.length === 1){
         profilePicture = data.images[0].url;
      }
   
-     // console.log(req.query.access_token) // access token in the url header to parse spotify data
       res.render("authentication/logged", {
         profilePicture: profilePicture,
         Name: data.display_name,
@@ -100,7 +80,7 @@ const constructorMethod = app => {
   })
 
   app.get("/UsTop50", async (req, res) => {
-      res.render("authentication/playlists");
+      res.render("authentication/playlists");   //this path never gets checked. Currently just rendered to see what is being done
   })
 
 
